529e95d312a1fe46e3c0541c698381a4f605dd1b
Fernando Hnerique Tozetto <fhtozetto@gmail.com>
2021-03-12 23:07:18 -0300

    Middlewares

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const { v4: uuidv4 } = require('uuid');
- 
 
 const app = express();
 
 app.use(express.json());
-
-const customers = [];
 
 /**
  * cpf - string
@@ -14,6 +11,23 @@ const customers = [];
  * id - uuid
  * statement []
  */
+const customers = [];
+
+//Middleware
+function verifyIfExistsAccountCPF(request, response, next) {
+    const { cpf } = request.headers;
+
+    const customer = customers.find(customer => customer.cpf === cpf);
+
+    if (!customer) {
+        return response.status(400).json({ error: 'Customer not found' });
+    }
+
+    request.customer = customer;
+
+    return next();
+}
+
 app.post('/account', (request, response) => {
     const { cpf, name } = request.body;
 
@@ -33,14 +47,10 @@ app.post('/account', (request, response) => {
     return response.status(201).send();
 });
 
-app.get('/statement/', (request, response) => {
-    const { cpf } = request.headers;
+//app.use(verifyIfExistsAccountCPF)/
 
-    const customer = customers.find(customer => customer.cpf === cpf);
-
-    if (!customer) {
-        return response.status(400).json({ error: 'Customer not found' });
-    }
+app.get('/statement', verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
 
     return response.json(customer.statement);
 });
